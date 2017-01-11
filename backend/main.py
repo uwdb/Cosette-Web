2a7f3f46d80592a7f5e8d6cb9ae78d2904cbc6ae
Shumo Chu <shumo.chu@gmail.com>
2017-01-10 22:16:48 -0800

    negative example works

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 # Main api routes for frontend
 
 import os
+import subprocess
 
 from flask import Flask, render_template, request
 template_dir = os.path.abspath('../frontend')
@@ -18,4 +19,23 @@ def index():
 @app.route('/solve', methods = ['POST'])
 def solve():
     query = request.form.get('query')
-    return query
+    qid = request.form.get("qid")
+    if qid == "1":
+        cmd = ['racket', '/Users/chushumo/Project/Cosette/rosette/tests/count-bug.rkt']
+        output = subprocess.Popen( cmd, stdout=subprocess.PIPE ).communicate()[0]
+        ret = "NOT EQUAL \n\n"
+        ret = ret + output.split('\n', 1)[0]
+        ret = ret + "\n\n"
+        ret = ret + "parts: \n"
+        ret = ret + "pnum | qoh | MUL  \n"
+        ret = ret + "================= \n"
+        ret = ret + "0    | 0   | 8    \n"
+        ret = ret + "2    | 2   | 15   \n"
+        ret = ret + "\n"
+        ret = ret + "supply:\n"
+        ret = ret + "pnum | shipdate | MUL \n"
+        ret = ret + "====================== \n"
+        ret = ret + "2    | 9        | 2    "
+        return ret
+    else:
+        return "I am not integers"
