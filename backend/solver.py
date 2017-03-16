da9a0784e2c239e13c111c18f748d26a9c5e1034
Daniel Li <dli123@uw.edu>
2017-03-16 02:29:09 -0700

    parsing queries

--- backend/solver.py
+++ backend/solver.py
@@ -7,6 +7,12 @@ def solve(query):
         temp.write(query);
         temp.seek(0);
         cmd = 'cd Cosette; ./solve.sh ' + temp.name
-        print cmd
         output = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT, universal_newlines=False)
-        return output
+        output_cmp = output.lower()
+        if "error" in output_cmp:
+            if "attempt to save an incomplete proof" in output_cmp:
+                return "Queries are not equivalent."
+            elif "syntax error" in output_cmp:
+                return "Syntax error in Cosette."
+        else:
+            return "Success. Queries are equivalent."
