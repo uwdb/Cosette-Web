8c86be1b73886ad26522264289ce4d8146531170
stechu <shumo.chu@gmail.com>
2017-04-30 15:39:59 -0700

    we don't really need submodule.

--- backend/rosette_FAKE.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sleep 1;
-
-echo '"{\"counter-example\":[{\"table-content\":[[\"id\"],[[[0],0],[[1],13],[[1],5]]],\"table-name\":\"t1\"},{\"table-content\":[[\"id\"],[]],\"table-name\":\"t2\"}],\"status\":\"neq\"}"'
