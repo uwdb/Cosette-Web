22f98ff943db50ea0b53c12aa6b2571986dcb0eb
stechu <shumo.chu@gmail.com>
2017-05-04 22:52:50 -0700

    fix database connection

--- backend/main.py
+++ backend/main.py
@@ -4,18 +4,14 @@ import os
 from flask import Flask, render_template, request, send_from_directory, abort
 import logging
 from logging.handlers import RotatingFileHandler
+import Cosette.solver as solver
+import psycopg2
+import time
+import json
 
 template_dir = os.path.abspath('../frontend')
 static_dir = os.path.abspath('../frontend/static')
 app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
-
-
-import Cosette.solver as solver
-
-import psycopg2
-
-import time
-
 
 # Index page for GET
 @app.route('/')
@@ -25,28 +21,36 @@ def index():
 # Solve api call
 @app.route('/solve', methods = ['POST'])
 def solve():
-    hostname = 'grabthar.cs.washington.edu'
-    username = os.environ['COS_DB_USERNAME']
-    password = os.environ['COS_DB_PASSWORD']
-    database = os.environ['COS_DB_DATABASE']
+    db_hostname = 'grabthar.cs.washington.edu'
+    db_username = os.environ['COS_DB_USERNAME']
+    db_password = os.environ['COS_DB_PASSWORD']
+    db_name = os.environ['COS_DB_DATABASE']
     if 'username' in request.cookies:
         username = request.cookies['username']
         query = request.form.get('query')
-        email = ''
+        email = 'None'
         if 'email' in request.cookies:
             email = request.cookies['email']
-        institution = ''
+        institution = 'None'
         if 'institution' in request.cookies:
             institution = request.cookies['institution']
         res = solver.solve(query, "./Cosette")
-        return res
-        conn = psycopg2.connect(host=hostname, user=username, password=password, dbname=database)
+        conn = psycopg2.connect(host=db_hostname, user=db_username,
+                                password=db_password, dbname=db_name)
         cur = conn.cursor()
-        cur.execute('INSERT INTO queries (username, institution, email, timestamp, cosette_code, result_json) VALUES (%s, %s, %s, %s, %s, %s)', (username, institution, email, time.time() * 1000, query, json.dumps(res)))
+        cur.execute('INSERT INTO queries (username, institution, email, timestamp, cosette_code, result) VALUES (%s, %s, %s, %s, %s, %s)',
+                    (username,
+                     institution,
+                     email,
+                     time.time() * 1000,
+                     query,
+                     json.dumps(res)))
+        conn.commit()
+        cur.close()
         conn.close()
         return res
     else:
-        return 'username: ' + username + ' ... password: ' + password + ' ... ' + database
+        # return 'username: ' + username + ' ... password: ' + password + ' ... ' + database
         abort(403)
 
 @app.route('/register', methods = ['POST'])
